de7164f46fab2e4b0d5ecb1f71d19c1569e8de0b
Villoro <villoro7@gmail.com>
2019-03-26 10:07:07 +0100

    [TST] install requirements in travis + test store_secret

--- test/test_encryption.py
+++ test/test_encryption.py
@@ -10,6 +10,11 @@ from v_crypt import encryption as enc
 class TestEncryption(unittest.TestCase):
     """Test encryption"""
 
+    def test_store_secret(self):
+        """ Test password creation with storing """
+
+        self.assertIsNotNone(enc.create_password(filename="temp.password"))
+
     def test_encrypt(self):
         """ Test that is able to encrypt a secret """
 
