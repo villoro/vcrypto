26099cbaf5de46c5222868a61ac2ab961a3a6d5c
villoro7 <villoro7@gmail.com>
2020-05-10 11:15:11 +0200

    update version

--- tests/test_vcrypto.py
+++ tests/test_vcrypto.py
@@ -4,4 +4,4 @@ from vcrypto import __version__
 
 
 def test_version():
-    assert __version__ == "1.0.4"
+    assert __version__ == "2.0.0"
--- vcrypto/__init__.py
+++ vcrypto/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "1.0.4"
+__version__ = "2.0.0"
 
 from .vcrypto import Cipher
