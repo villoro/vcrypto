8cff76aebbfe94a8d498f206f2f40b1a7771e5e5
Arnau Villoro <arnau.villoro@northius.com>
2025-02-24 07:37:27 +0100

    Improve definition

--- vcrypto/__init__.py
+++ vcrypto/__init__.py
@@ -1,6 +1,19 @@
-from vcrypto.vcrypto import init_vcrypto
-from vcrypto.vcrypto import get_secret
-from vcrypto.vcrypto import get_secret as read_secret
-from vcrypto.vcrypto import save_secret
 from vcrypto.vcrypto import create_password
 from vcrypto.vcrypto import export_secret
+from vcrypto.vcrypto import get_secret
+from vcrypto.vcrypto import init_vcrypto
+from vcrypto.vcrypto import save_secret
+
+
+# Alias `get_secret` as `read_secret`
+read_secret = get_secret
+
+# Explicitly define public API
+__all__ = [
+    "init_vcrypto",
+    "get_secret",
+    "read_secret",
+    "save_secret",
+    "create_password",
+    "export_secret",
+]
